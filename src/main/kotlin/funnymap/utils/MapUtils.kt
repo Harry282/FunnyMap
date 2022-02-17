32f868175d9623907996eac2031a40f8dff6f03c
Harry282 <iamharry282@gmail.com>
2022-02-17 12:01:47 -0500

    Utils for non-mcp-mapped functions

--- src/main/kotlin/funnymap/utils/MapUtils.kt
+++ src/main/kotlin/funnymap/utils/MapUtils.kt
@@ -2,6 +2,7 @@ package funnymap.utils
 
 import funnymap.FunnyMap.Companion.mc
 import net.minecraft.item.ItemMap
+import net.minecraft.util.Vec4b
 import net.minecraft.world.storage.MapData
 
 object MapUtils {
@@ -15,4 +16,13 @@ object MapUtils {
         if (map.item !is ItemMap || !map.displayName.contains("Magical Map")) return null
         return (map.item as ItemMap).getMapData(map, mc.theWorld)
     }
+
+    val Vec4b.mapX
+        get() = (this.func_176112_b() + 128) shr 1
+
+    val Vec4b.mapZ
+        get() = (this.func_176113_c() + 128) shr 1
+
+    val Vec4b.yaw
+        get() = this.func_176111_d() * 22.5f
 }
