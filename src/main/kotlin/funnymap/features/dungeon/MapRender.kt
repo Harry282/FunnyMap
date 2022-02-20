24ed3b509ff31b3b8e242cb1438c91a7fc80a925
Harry282 <iamharry282@gmail.com>
2022-02-20 12:47:44 -0500

    Overhaul map rendering
    Now is scaled the same as the in-game map item.

--- src/main/kotlin/funnymap/features/dungeon/MapRender.kt
+++ src/main/kotlin/funnymap/features/dungeon/MapRender.kt
@@ -4,6 +4,8 @@ import funnymap.FunnyMap.Companion.config
 import funnymap.FunnyMap.Companion.inDungeons
 import funnymap.FunnyMap.Companion.mc
 import funnymap.core.*
+import funnymap.utils.MapUtils
+import funnymap.utils.MapUtils.roomSize
 import net.minecraft.client.gui.Gui
 import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.entity.player.EnumPlayerModelParts
@@ -24,31 +26,33 @@ object MapRender {
 
     @SubscribeEvent
     fun onOverlay(event: RenderGameOverlayEvent.Post) {
-        if (event.type != RenderGameOverlayEvent.ElementType.HOTBAR || !inDungeons || !config.mapEnabled) return
+        if (event.type != RenderGameOverlayEvent.ElementType.ALL || !inDungeons || !config.mapEnabled) return
         if (config.mapHideInBoss && Dungeon.inBoss || !Dungeon.hasScanned) return
         if (mc.currentScreen is MoveMapGui) return
 
-        renderRooms()
-
-        renderText()
-
-        renderPlayerHeads()
-    }
-
-    fun renderRooms() {
-        val mapScale = config.mapScale
-
         GlStateManager.pushMatrix()
         GlStateManager.translate(config.mapX.toFloat(), config.mapY.toFloat(), 0f)
-        GlStateManager.scale(mapScale, mapScale, 1f)
+        GlStateManager.scale(config.mapScale, config.mapScale, 1f)
+        GlStateManager.translate(MapUtils.startCorner.first.toFloat(), MapUtils.startCorner.second.toFloat(), 0f)
+
+        renderRooms()
+        renderText()
+        renderPlayerHeads()
+
+        GlStateManager.popMatrix()
+    }
+
+
+    fun renderRooms() {
+        val connectorSize = roomSize shr 2
 
         for (y in 0..10) {
             for (x in 0..10) {
                 val tile = Dungeon.dungeonList[y * 11 + x]
                 if (tile is Door && tile.type == DoorType.NONE) continue
 
-                val xOffset = (x shr 1) * 26
-                val yOffset = (y shr 1) * 26
+                val xOffset = (x shr 1) * (roomSize + connectorSize)
+                val yOffset = (y shr 1) * (roomSize + connectorSize)
 
                 val xEven = x and 1 == 0
                 val yEven = y and 1 == 0
@@ -70,24 +74,27 @@ object MapRender {
                         Gui.drawRect(
                             xOffset + roomSize,
                             yOffset + roomSize,
-                            xOffset + 26,
-                            yOffset + 26,
+                            xOffset + (roomSize + connectorSize),
+                            yOffset + (roomSize + connectorSize),
                             color.rgb
                         )
                     }
-                    else -> drawRoomConnector(xOffset, yOffset, tile is Door, !xEven, color.rgb)
+                    else -> drawRoomConnector(
+                        xOffset,
+                        yOffset,
+                        connectorSize,
+                        tile is Door,
+                        !xEven,
+                        color.rgb
+                    )
                 }
             }
         }
 
-        GlStateManager.popMatrix()
     }
 
     private fun renderText() {
-        GlStateManager.pushMatrix()
-        GlStateManager.translate(config.mapX.toFloat(), config.mapY.toFloat(), 0f)
-        GlStateManager.scale(config.mapScale, config.mapScale, 1f)
-
+        val connectorSize = roomSize shr 2
 
         for (y in 0..10 step 2) {
             for (x in 0..10 step 2) {
@@ -96,25 +103,18 @@ object MapRender {
 
                 if (tile is Room && tile in Dungeon.uniqueRooms) {
 
-                    val xOffset = (x shr 1) * 26
-                    val yOffset = (y shr 1) * 26
+                    val xOffset = (x shr 1) * (roomSize + connectorSize)
+                    val yOffset = (y shr 1) * (roomSize + connectorSize)
 
                     if (config.mapCheckmark != 0 && config.mapRoomSecrets != 2) {
                         getCheckmark(tile.state, config.mapCheckmark)?.let {
                             GlStateManager.enableAlpha()
                             GlStateManager.color(255f, 255f, 255f, 255f)
                             mc.textureManager.bindTexture(it)
-                            when (config.mapCheckmark) {
-                                1 -> Gui.drawModalRectWithCustomSizedTexture(
-                                    xOffset + 4, yOffset + 4,
-                                    0f, 0f, 12, 12, 12f, 12f
-                                )
-                                2 -> Gui.drawModalRectWithCustomSizedTexture(
-                                    xOffset + 2, yOffset + 2,
-                                    0f, 0f, 16, 16, 16f, 16f
-                                )
-                                else -> {}
-                            }
+                            Gui.drawModalRectWithCustomSizedTexture(
+                                xOffset + 2, yOffset + 2,
+                                0f, 0f, roomSize - 4, roomSize - 4, roomSize - 4f, roomSize - 4f
+                            )
                             GlStateManager.disableAlpha()
                         }
                     }
@@ -137,12 +137,10 @@ object MapRender {
                     } else 0xffffff
 
                     // Offset + half of roomsize
-                    drawCenteredText(name, xOffset + 10f, yOffset + 10f, color)
+                    drawCenteredText(name, xOffset + (roomSize shr 1), yOffset + (roomSize shr 1), color)
                 }
             }
         }
-
-        GlStateManager.popMatrix()
     }
 
     private fun getCheckmark(state: RoomState, type: Int): ResourceLocation? {
@@ -164,24 +162,25 @@ object MapRender {
     }
 
     private fun renderPlayerHeads() {
+        val multiplier = (roomSize + 4.0) / Dungeon.roomSize
         for (player in Dungeon.dungeonTeamates) {
-            drawPlayerHead(player)
+            drawPlayerHead(player, multiplier)
         }
     }
 
-    private fun drawCenteredText(text: List<String>, x: Float, y: Float, color: Int) {
+    private fun drawCenteredText(text: List<String>, x: Int, y: Int, color: Int) {
         GlStateManager.pushMatrix()
 
-        GlStateManager.translate(x, y, 0f)
+        GlStateManager.translate(x.toFloat(), y.toFloat(), 0f)
         GlStateManager.scale(config.textScale, config.textScale, 1f)
 
         if (text.isNotEmpty()) {
-            val yTextOffset = 5f * text.size
+            val yTextOffset = text.size * 5f
             for (i in text.indices) {
                 mc.fontRendererObj.drawString(
                     text[i],
-                    -mc.fontRendererObj.getStringWidth(text[i]) / 2f,
-                    -yTextOffset + i * 10f,
+                    (-mc.fontRendererObj.getStringWidth(text[i]) shr 1).toFloat(),
+                    i * 10 - yTextOffset,
                     color,
                     true
                 )
@@ -190,21 +189,20 @@ object MapRender {
         GlStateManager.popMatrix()
     }
 
-    private fun drawPlayerHead(player: DungeonPlayer) {
+    private fun drawPlayerHead(player: DungeonPlayer, multiplier: Double) {
         GlStateManager.pushMatrix()
         try {
             GlStateManager.translate(
-                ((player.x - Dungeon.startX) / Dungeon.roomSize * 26 + 8) * config.mapScale + config.mapX,
-                ((player.z - Dungeon.startZ) / Dungeon.roomSize * 26 + 8) * config.mapScale + config.mapY,
+                (player.x - Dungeon.startX) * multiplier + 8,
+                (player.z - Dungeon.startZ) * multiplier + 8,
                 0.0
             )
             GlStateManager.rotate(player.yaw + 180f, 0f, 0f, 1f)
+            GlStateManager.scale(config.playerHeadScale, config.playerHeadScale, 1f)
             mc.textureManager.bindTexture(mc.netHandler.getPlayerInfo(player.player.uniqueID).locationSkin)
-            val pos = (-6 * config.mapScale).toInt()
-            val size = (12 * config.mapScale).toInt()
-            Gui.drawScaledCustomSizeModalRect(pos, pos, 8f, 8f, 8, 8, size, size, 64f, 64f)
+            Gui.drawScaledCustomSizeModalRect(-6, -6, 8f, 8f, 8, 8, 12, 12, 64f, 64f)
             if (player.player.isWearing(EnumPlayerModelParts.HAT)) {
-                Gui.drawScaledCustomSizeModalRect(pos, pos, 40f, 8f, 8, 8, size, size, 64f, 64f)
+                Gui.drawScaledCustomSizeModalRect(-6, -6, 40f, 8f, 8, 8, 12, 12, 64f, 64f)
             }
         } catch (e: Exception) {
             e.printStackTrace()
@@ -212,18 +210,23 @@ object MapRender {
         GlStateManager.popMatrix()
     }
 
-    private fun drawRoomConnector(x: Int, y: Int, doorway: Boolean, vertical: Boolean, color: Int) {
-        // 8 x 12 or 8 x 20 connector
-        val doorwayOffset = 6
-        val length = 6
-        val width = if (doorway) 8 else roomSize
+    private fun drawRoomConnector(
+        x: Int,
+        y: Int,
+        doorWidth: Int,
+        doorway: Boolean,
+        vertical: Boolean,
+        color: Int
+    ) {
+        val doorwayOffset = if (roomSize == 16) 5 else 6
+        val width = if (doorway) 6 else roomSize
         var x1 = if (vertical) x + roomSize else x
         var y1 = if (vertical) y else y + roomSize
         if (doorway) {
             if (vertical) y1 += doorwayOffset else x1 += doorwayOffset
         }
-        val x2 = if (vertical) x1 + length else x1 + width
-        val y2 = if (vertical) y1 + width else y1 + length
+        val x2 = if (vertical) x1 + doorWidth else x1 + width
+        val y2 = if (vertical) y1 + width else y1 + doorWidth
         Gui.drawRect(x1, y1, x2, y2, color)
     }
 }
