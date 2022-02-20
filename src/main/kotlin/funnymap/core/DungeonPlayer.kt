cd46ce1bd73d658b5d5bf922c7d86e1e3edf3eed
Harry282 <iamharry282@gmail.com>
2022-02-20 12:42:34 -0500

    Update DungeonPlayer.kt

--- src/main/kotlin/funnymap/core/DungeonPlayer.kt
+++ src/main/kotlin/funnymap/core/DungeonPlayer.kt
@@ -2,7 +2,7 @@ package funnymap.core
 
 import net.minecraft.entity.player.EntityPlayer
 
-class DungeonPlayer(val player: EntityPlayer, val name: String) {
+data class DungeonPlayer(val player: EntityPlayer, val name: String) {
     var x = 0.0
     var z = 0.0
     var yaw = 0f
