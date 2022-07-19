2c7961c181637f7d0540dcd57b3ba1847da5a927
Harry282 <iamharry282@gmail.com>
2022-07-19 08:50:34 -0400

    Bump dependencies, use Skytils' ForgeGradle
    (cherry picked from commit 8a36e01937820569858453b32901104e9df3209f)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
-                "net.minecraftforge.gradle.forge" -> useModule("com.github.asbyth:ForgeGradle:${requested.version}")
+                "net.minecraftforge.gradle.forge" -> useModule("com.github.Skytils:ForgeGradle:${requested.version}")
                 "org.spongepowered.mixin" -> useModule("com.github.xcfrg:MixinGradle:${requested.version}")
             }
         }
