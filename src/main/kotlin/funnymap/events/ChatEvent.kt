44c51cbc5beb6a696d047c3897da750e44f7aae7
Harry282 <iamharry282@gmail.com>
2022-11-29 22:19:50 -0500

    by lazy
    (cherry picked from commit 854cf5305540c5af1d9c899d41e70af98486b55e)

--- src/main/kotlin/funnymap/events/ChatEvent.kt
+++ src/main/kotlin/funnymap/events/ChatEvent.kt
@@ -5,5 +5,5 @@ import net.minecraft.util.StringUtils
 import net.minecraftforge.fml.common.eventhandler.Event
 
 class ChatEvent(val packet: S02PacketChat) : Event() {
-    val text: String = StringUtils.stripControlCodes(packet.chatComponent.unformattedText)
+    val text: String by lazy { StringUtils.stripControlCodes(packet.chatComponent.unformattedText) }
 }
