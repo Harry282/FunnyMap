1a58954dc6b7e4d1d4f35316444321d97830715f
Harry282 <iamharry282@gmail.com>
2023-02-07 22:08:16 -0500

    Bump version

--- src/main/kotlin/funnymap/FunnyMap.kt
+++ src/main/kotlin/funnymap/FunnyMap.kt
@@ -66,7 +66,7 @@ class FunnyMap {
     companion object {
         const val MOD_ID = "funnymap"
         const val MOD_NAME = "Funny Map"
-        const val MOD_VERSION = "0.6.4"
+        const val MOD_VERSION = "0.7.0"
         const val CHAT_PREFIX = "§b§l<§fFunny Map§b§l>§r"
 
         val mc: Minecraft = Minecraft.getMinecraft()
