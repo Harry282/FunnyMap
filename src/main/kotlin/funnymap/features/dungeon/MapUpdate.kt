a3f6dda978614e439b1b7162d205bb565c48187a
Harry282 <iamharry282@gmail.com>
2023-10-20 14:14:23 -0400

    Avoid multiple icons set as isPlayer
    (cherry picked from commit 40c35fb523b79933c2609e4e6b6efafae31a7048)

--- src/main/kotlin/funnymap/features/dungeon/MapUpdate.kt
+++ src/main/kotlin/funnymap/features/dungeon/MapUpdate.kt
@@ -80,20 +80,18 @@ object MapUpdate {
 
         val decor = MapUtils.getMapData()?.mapDecorations ?: return
         Dungeon.dungeonTeammates.forEach { (name, player) ->
-            if (player.isPlayer || name == mc.thePlayer.name) {
-                player.isPlayer = true
-                player.yaw = mc.thePlayer.rotationYaw
-                player.mapX =
-                    ((mc.thePlayer.posX - DungeonScan.startX + 15) * MapUtils.coordMultiplier + MapUtils.startCorner.first).roundToInt()
-                player.mapZ =
-                    ((mc.thePlayer.posZ - DungeonScan.startZ + 15) * MapUtils.coordMultiplier + MapUtils.startCorner.second).roundToInt()
-                return@forEach
-            }
             decor.entries.find { (icon, _) -> icon == player.icon }?.let { (_, vec4b) ->
                 player.isPlayer = vec4b.func_176110_a().toInt() == 1
                 player.mapX = vec4b.mapX
                 player.mapZ = vec4b.mapZ
                 player.yaw = vec4b.yaw
+            }
+            if (player.isPlayer || name == mc.thePlayer.name) {
+                player.yaw = mc.thePlayer.rotationYaw
+                player.mapX =
+                    ((mc.thePlayer.posX - DungeonScan.startX + 15) * MapUtils.coordMultiplier + MapUtils.startCorner.first).roundToInt()
+                player.mapZ =
+                    ((mc.thePlayer.posZ - DungeonScan.startZ + 15) * MapUtils.coordMultiplier + MapUtils.startCorner.second).roundToInt()
             }
         }
     }
