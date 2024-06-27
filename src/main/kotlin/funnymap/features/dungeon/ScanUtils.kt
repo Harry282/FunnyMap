44a1958158a2c1849a9d465891c1374da591b299
Harry282 <iamharry282@gmail.com>
2024-06-27 01:33:13 -0400

    More optimized core scanning

--- src/main/kotlin/funnymap/features/dungeon/ScanUtils.kt
+++ src/main/kotlin/funnymap/features/dungeon/ScanUtils.kt
@@ -51,12 +51,25 @@ object ScanUtils {
 
     fun getCore(x: Int, z: Int): Int {
         val sb = StringBuilder(150)
-        val chunk = mc.theWorld.getChunkFromBlockCoords(BlockPos(x, 0, z))
-        for (y in 140 downTo 12) {
-            val id = Block.blockRegistry.getIDForObject(chunk.getBlock(BlockPos(x, y, z)))
-            if (!id.equalsOneOf(5, 54, 146)) {
-                sb.append(id)
+        val chunk = mc.theWorld.getChunkFromChunkCoords(x shr 4, z shr 4)
+        val height = chunk.getHeightValue(x and 15, z and 15).coerceIn(11..140)
+        sb.append(CharArray(140 - height) { '0' })
+        var bedrock = 0
+        for (y in height downTo 12) {
+            val id = Block.getIdFromBlock(chunk.getBlock(BlockPos(x, y, z)))
+            if (id == 0 && bedrock >= 2 && y < 69) {
+                sb.append(CharArray(y - 11) { '0' })
+                break
             }
+
+            if (id == 7) {
+                bedrock++
+            } else {
+                bedrock = 0
+                if (id.equalsOneOf(5, 54, 146)) continue
+            }
+
+            sb.append(id)
         }
         return sb.toString().hashCode()
     }
